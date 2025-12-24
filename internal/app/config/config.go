3a65df2e962c5bc02db70d7034682f25611033fc
popavsya <iaforism@gmail.com>
2025-12-24 21:05:07 +0300

    TASK-1001: Отрефакторил расположение структур

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -4,28 +4,14 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"time"
 
+	"github.com/iFreezy/catalog-service/internal/app/config/section"
 	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 )
 
 type Config struct {
-	Repository Repository
-}
-
-type Repository struct {
-	Postgres RepositoryPostgres
-}
-
-type RepositoryPostgres struct {
-	Address        string        `envconfig:"APP_REPOSITORY_POSTGRES_ADDRESS"             default:"localhost:5429"`
-	Username       string        `envconfig:"APP_REPOSITORY_POSTGRES_USERNAME"`
-	Password       string        `envconfig:"APP_REPOSITORY_POSTGRES_PASSWORD"`
-	Name           string        `envconfig:"APP_REPOSITORY_POSTGRES_NAME"`
-	MigrationTable string        `envconfig:"APP_REPOSITORY_POSTGRES_MIGRATION_TABLE"     default:"schema_migrations"`
-	ReadTimeout    time.Duration `envconfig:"APP_REPOSITORY_POSTGRES_READ_TIMEOUT"        default:"30s"`
-	WriteTimeout   time.Duration `envconfig:"APP_REPOSITORY_POSTGRES_WRITE_TIMEOUT"       default:"30s"`
+	Repository section.Repository
 }
 
 func Load() (Config, error) {
---
+++ internal/app/config/section/repository.go
@@ -0,0 +1,5 @@
+package section
+
+type Repository struct {
+	Postgres RepositoryPostgres
+}
